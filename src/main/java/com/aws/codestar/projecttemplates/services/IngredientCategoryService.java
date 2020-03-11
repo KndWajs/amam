928bad9dae9e71de32b757bb2b5ec52538a0dc08
konwajs <konrad.wajs@capgemini.com>
2020-03-11 17:31:53 +0100

    add validation to IngredientService

--- src/main/java/com/aws/codestar/projecttemplates/services/IngredientCategoryService.java
+++ src/main/java/com/aws/codestar/projecttemplates/services/IngredientCategoryService.java
@@ -1,10 +1,13 @@
 package com.aws.codestar.projecttemplates.services;
 
 import com.aws.codestar.projecttemplates.dto.IngredientCategoryDTO;
+import com.aws.codestar.projecttemplates.dto.IngredientDTO;
+import com.aws.codestar.projecttemplates.exceptions.EmptyRequiredFieldException;
+import com.aws.codestar.projecttemplates.exceptions.ObjectIsNullException;
 import com.aws.codestar.projecttemplates.mappers.IngredientCategoryMapper;
 import com.aws.codestar.projecttemplates.persistence.entities.IngredientCategory;
 import com.aws.codestar.projecttemplates.persistence.repositories.IngredientCategoryRepository;
-import org.modelmapper.ModelMapper;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -34,8 +37,18 @@ public class IngredientCategoryService {
     }
 
     public IngredientCategoryDTO addNewIngredientCategory(IngredientCategoryDTO ingredientCategoryDTO) {
+        validateIngredientCategoryObject(ingredientCategoryDTO);
         IngredientCategory newIngredientCategory = new IngredientCategory();
         newIngredientCategory.setCategory(ingredientCategoryDTO.getCategory().toLowerCase());
         return ingredientCategoryMapper.toDTO(ingredientCategoryRepository.save(newIngredientCategory));
+    }
+
+    private void validateIngredientCategoryObject(IngredientCategoryDTO ingredientCategory) {
+        if (ingredientCategory == null) {
+            throw new ObjectIsNullException(IngredientDTO.class.getName());
+        }
+        if (StringUtils.isEmpty(ingredientCategory.getCategory())) {
+            throw new EmptyRequiredFieldException("Category");
+        }
     }
 }
