00878aeffd1fead4c7915272720b29292676dc52
konwajs <konrad.wajs@capgemini.com>
2020-04-09 23:00:16 +0200

    test controller

--- src/main/java/pl/fit_amam/api/controllers/HelloWorldController.java
+++ src/main/java/pl/fit_amam/api/controllers/HelloWorldController.java
@@ -1,13 +1,21 @@
 package pl.fit_amam.api.controllers;
 
-import pl.fit_amam.api.Globals;
-import pl.fit_amam.api.persistence.repositories.MealIngredientDao;
+import com.nimbusds.jwt.JWTClaimsSet;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
+import pl.fit_amam.api.Globals;
+import pl.fit_amam.api.persistence.repositories.MealIngredientDao;
+import pl.fit_amam.api.security.cognitojwt.AwsJwtValidator;
+import pl.fit_amam.api.security.cognitojwt.CognitoAuthenticationToken;
+import pl.fit_amam.api.security.cognitojwt.TokenException;
+
+import java.io.IOException;
+import java.util.Map;
 
 /**
  * Basic Spring web service controller that handles all GET requests.
@@ -34,4 +42,25 @@ public class HelloWorldController {
         return new JSONObject().put("Output", text).toString();
     }
 
+
+    //TODO for testing purpose - remove this later
+    @GetMapping(path = "/get-user-info/")
+    @ResponseBody
+    public String test(@RequestHeader("Authorization") String token) throws IOException, TokenException {
+
+        JWTClaimsSet clset = AwsJwtValidator.validateAWSJwtToken(token);
+
+        String claims = "";
+        for (Map.Entry<String, Object> claim :clset.getClaims().entrySet()) {
+            claims = claims + "\n" + claim.getKey() + ": " + claim.getValue().toString() ;
+        }
+
+        CognitoAuthenticationToken ct = (CognitoAuthenticationToken) SecurityContextHolder.getContext().getAuthentication();
+
+        return "User data taken from token: \n"
+                + claims + "\n"
+                + ct.getPrincipal().getClaim("email").toString() + "\n"
+                + ct.getPrincipal().getExpirationTime() + "\n"
+                ;
+    }
 }
