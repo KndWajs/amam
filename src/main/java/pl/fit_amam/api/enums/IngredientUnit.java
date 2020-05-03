3dc46b636caa8cd4520ae4426ab56995882ff8f9
KndWajs <knd.wajs@gmail.com>
2020-05-03 18:16:46 +0200

    add description to unit enum

--- src/main/java/pl/fit_amam/api/enums/IngredientUnit.java
+++ src/main/java/pl/fit_amam/api/enums/IngredientUnit.java
@@ -1,6 +1,16 @@
 package pl.fit_amam.api.enums;
 
 public enum IngredientUnit {
-    ML_OR_GRAM,
-    PCS
+    ML_OR_GRAM("ml or gram"),
+    PCS("pcs");
+
+    private String description;
+
+    IngredientUnit( String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
 }
