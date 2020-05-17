076b7d064974ebbdad7b5237f42956d9c78d9882
KndWajs <knd.wajs@gmail.com>
2020-05-17 23:36:37 +0200

    bugfix

--- src/main/java/pl/fit_amam/api/security/springsecurity/WebSecurityConfig.java
+++ src/main/java/pl/fit_amam/api/security/springsecurity/WebSecurityConfig.java
@@ -3,11 +3,12 @@ package pl.fit_amam.api.security.springsecurity;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 
 @Configuration
-//@EnableWebSecurity
+@EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 
@@ -19,7 +20,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers(HttpMethod.PUT, "/**").not().hasAuthority("guest")
                 .antMatchers(HttpMethod.DELETE, "/**").not().hasAuthority("guest")
                 .and()
-//                .addFilter(new AuthFilter(authenticationManager()))
+                .addFilter(new AuthFilter(authenticationManager()))
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
     }
 }
